87e6dc7fb4a609e4de9c81ce7ae709c77ff2b118
Ethan  Ratnarajah <ethanratnarajah@MacBook-Pro-de-Ethan.local>
2017-11-05 12:58:46 +0100

    modify README

---
+++ TestApiASPNETCore/Controllers/DBController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TestApiASPNETCore.Models;
+
+namespace TestApiASPNETCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class DBController : Controller
+    {
+        // GET api/db
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            using (var db = new PersonsContext())
+            {
+                var persons = db.persons.OrderBy(p => p.Id).ToList<Person>();
+                List<string> test = persons.Select(p => p.Name).ToList();
+                return test;
+            }
+        }
+    }
+}
---
+++ TestApiASPNETCore/Models/PersonsContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestApiASPNETCore.Models
+{
+    public class PersonsContext : DbContext
+
+    {
+        public DbSet<Person> persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"Server=localhost;Port=5432;Database=DBTest;User Id=test;Password=test;");
+        }
+    }
+
+    [Table("persons")]
+    public class Person
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+    }
+}
